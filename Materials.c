bb438ffa08cfbf5307e6ab7d657f7371027dd656
John Tramm <john.tramm@gmail.com>
2012-11-01 12:44:33 -0500

    cleaned up mats

--- Materials.c
+++ Materials.c
@@ -27,13 +27,13 @@ int ** load_mats( int * num_nucs )
 	
 	int mats0[] =  { 58, 59, 60, 61, 40, 42, 43, 44, 45, 46, 1, 2, 3, 7,
 	                 8, 9, 10, 29, 57, 47, 48, 0, 62, 15, 33, 34, 52, 53,
-									 54, 55, 56, 18, 23, 41, -1 }; //fuel
+	                 54, 55, 56, 18, 23, 41, -1 }; //fuel
 	int mats1[] =  { 63, 64, 65, 66, 67, -1 }; // cladding
 	int mats2[] =  { 24, 41, 4, 5, -1 }; // cold borated water
 	int mats3[] =  { 24, 41, 4, 5, -1 }; // hot borated water
 	int mats4[] =  { 19, 20, 21, 22, 35, 36, 37, 38, 39, 25, 27, 28, 29,
 	                 30, 31, 32, 26, 49, 50, 51, 11, 12, 13, 14, 6, 16,
-									 17, -1 }; // RPV
+	                 17, -1 }; // RPV
 	int mats5[] =  { 24, 41, 4, 5, 19, 20, 21, 22, 35, 36, 37, 38, 39, 25,
 	                 49, 50, 51, 11, 12, 13, 14, -1 }; // lower radial reflector
 	int mats6[] =  { 24, 41, 4, 5, 19, 20, 21, 22, 35, 36, 37, 38, 39, 25,
@@ -47,18 +47,18 @@ int ** load_mats( int * num_nucs )
 	int mats10[] = { 24, 41, 4, 5, 63, 64, 65, 66, 67, -1 }; // top of fuel assemblies
 	int mats11[] = { 24, 41, 4, 5, 63, 64, 65, 66, 67, -1 }; // bottom of fuel assemblies
 
-	memcpy( mats[0], mats0, num_nucs[0]*sizeof(int) );	
-	memcpy( mats[1], mats1, num_nucs[1]*sizeof(int) );	
-	memcpy( mats[2], mats2, num_nucs[2]*sizeof(int) );	
-	memcpy( mats[3], mats3, num_nucs[3]*sizeof(int) );	
-	memcpy( mats[4], mats4, num_nucs[4]*sizeof(int) );	
-	memcpy( mats[5], mats5, num_nucs[5]*sizeof(int) );	
-	memcpy( mats[6], mats6, num_nucs[6]*sizeof(int) );	
-	memcpy( mats[7], mats7, num_nucs[7]*sizeof(int) );	
-	memcpy( mats[8], mats8, num_nucs[8]*sizeof(int) );	
-	memcpy( mats[9], mats9, num_nucs[9]*sizeof(int) );	
-	memcpy( mats[10], mats10, num_nucs[10]*sizeof(int) );	
-	memcpy( mats[11], mats11, num_nucs[11]*sizeof(int) );	
+	memcpy( mats[0],  mats0,  num_nucs[0]  * sizeof(int) );	
+	memcpy( mats[1],  mats1,  num_nucs[1]  * sizeof(int) );	
+	memcpy( mats[2],  mats2,  num_nucs[2]  * sizeof(int) );	
+	memcpy( mats[3],  mats3,  num_nucs[3]  * sizeof(int) );	
+	memcpy( mats[4],  mats4,  num_nucs[4]  * sizeof(int) );	
+	memcpy( mats[5],  mats5,  num_nucs[5]  * sizeof(int) );	
+	memcpy( mats[6],  mats6,  num_nucs[6]  * sizeof(int) );	
+	memcpy( mats[7],  mats7,  num_nucs[7]  * sizeof(int) );	
+	memcpy( mats[8],  mats8,  num_nucs[8]  * sizeof(int) );	
+	memcpy( mats[9],  mats9,  num_nucs[9]  * sizeof(int) );	
+	memcpy( mats[10], mats10, num_nucs[10] * sizeof(int) );	
+	memcpy( mats[11], mats11, num_nucs[11] * sizeof(int) );	
 
 	return mats;
 }
