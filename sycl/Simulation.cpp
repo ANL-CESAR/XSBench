9584399142bf4c753bdef57e2a41fa66c788e24e
Bryce Allen <bdallen@uchicago.edu>
2023-07-13 13:14:38 -0500

    sycl: update for SYCL2020 spec
    Works with oneAPI 2023.2 release, openSYCL

--- sycl/Simulation.cpp
+++ sycl/Simulation.cpp
@@ -12,7 +12,7 @@
 ////////////////////////////////////////////////////////////////////////////////////
 
 // use SYCL namespace to reduce symbol names
-using namespace cl::sycl;
+using namespace sycl;
 unsigned long long run_event_based_simulation(Inputs in, SimulationData SD, int mype, double * kernel_init_time)
 {
 	
@@ -51,7 +51,7 @@ unsigned long long run_event_based_simulation(Inputs in, SimulationData SD, int 
 		queue sycl_q{default_selector()};
 		//queue sycl_q{gpu_selector()};
 		//queue sycl_q{cpu_selector()};
-		if(mype == 0 ) printf("Running on: %s\n", sycl_q.get_device().get_info<cl::sycl::info::device::name>().c_str());
+		if(mype == 0 ) printf("Running on: %s\n", sycl_q.get_device().get_info<sycl::info::device::name>().c_str());
 		if(mype == 0 ) printf("Initializing device buffers and JIT compiling kernel...\n");
 	
 		////////////////////////////////////////////////////////////////////////////////
@@ -84,7 +84,7 @@ unsigned long long run_event_based_simulation(Inputs in, SimulationData SD, int 
 		// For the unionized grid, this is a large array. Enforce that it is able to be allocated on the
 		// OpenCL device (as some OpenCL devices have fairly low limts here for some reason...)
 		size_t index_grid_allocation_sz = ceil((SD.length_index_grid * sizeof(int)));
-		assert( index_grid_allocation_sz <= sycl_q.get_device().get_info<cl::sycl::info::device::max_mem_alloc_size>() );
+		assert( index_grid_allocation_sz <= sycl_q.get_device().get_info<sycl::info::device::max_mem_alloc_size>() );
 		buffer<int, 1> index_grid_d(SD.index_grid, (unsigned long long ) SD.length_index_grid);
 		
 		////////////////////////////////////////////////////////////////////////////////
